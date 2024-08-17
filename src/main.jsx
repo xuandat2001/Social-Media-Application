969c0b5334a5d8550356ce2ab45c496d6b3490dc
ĐẠT <ungxuandat11092001@gmail.com>
2024-08-17 17:29:28 +0700

    delete index.css, app.css and app.jsx, complete Header componet

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Group from './pages/Group.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Group/>
   </StrictMode>,
 )
