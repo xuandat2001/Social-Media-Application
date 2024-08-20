73ba72b80d95428058c85801efcffd9b84dda414
ĐẠT <ungxuandat11092001@gmail.com>
2024-08-20 23:25:04 +0700

    update group page

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -3,7 +3,7 @@ import Post from '../components/Post.jsx';
 import testImage from '../image/Screenshot 2024-08-12 000128.png'
 import groupImage from '../image/yellow-dog-image.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe,faLock } from '@fortawesome/free-solid-svg-icons';
 import '../css/group.css';
 function Group(){
     const posts = [
@@ -44,27 +44,30 @@ function Group(){
       groupMember: 10
     }
     return(
-      <>
-            <Header/>
+      <div className="group-page">
+          <Header/>
+          <div className="container">
             <div className="header-group">
-              <div className="img-group">
-                <img src={group.groupImage}/>
-              </div>
-              <div className="row">
-                <div className="col-10">
-                  <h1>{group.groupName}</h1>
-                  <p>Private Group {group.groupMember} members</p>
+                <div className="img-group">
+                  <img src={group.groupImage}/>
                 </div>
-                <div className="col-2"></div>
+                <div className="row">
+                  <div className="col-10">
+                    <h1 className="group-name">{group.groupName}</h1>
+                    <p className="group-detail"> <span><FontAwesomeIcon icon={faLock} /></span>Private Group {group.groupMember} members</p>
+                  </div>
+                  <div className="col-2"></div>
+                </div>
+                <div className="nav-group group-nav">
+                  <nav className="nav">
+                    <a className="nav-link" href="#">Discussion</a>
+                    <a className="nav-link" href="#">Member</a>
+                    <a className="nav-link" href="#">Media</a>
+                  </nav>
+                </div>
+
               </div>
-              <div className="nav-group">
-                <ul class="list-group">
-                  <li class="list-group-item">Discussion</li>
-                  <li class="list-group-item">Member</li>
-                  <li class="list-group-item">Media</li>
-                </ul>
-              </div>
-            </div>
+          </div>
           <div className="container-fluid">
 
             <div className="row">
@@ -84,7 +87,7 @@ function Group(){
      
             
         </div>
-      </>
+      </div>
 
     )
 }
