3c805129fcb8d6cecce2cfa0321a7eeab9f25c41
ĐẠT <ungxuandat11092001@gmail.com>
2024-08-23 03:03:05 +0700

    update reaction into comment like button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Reaction from "./Reaction";
+
 import '../css/componentItem.css';
 function CommentItem({imageUser,userName, content, numberOfReact = 1}){
+    const [showReactions, setShowReactions] = useState(false);
     return(
         <>
             <div className="comment-list">
@@ -11,9 +14,13 @@ function CommentItem({imageUser,userName, content, numberOfReact = 1}){
                     <div className="col-11">
                         <h4>{userName}</h4>
                         <p className="host-comment">{content}</p>
-                        <div className="comment-detail">
+                        <div className="comment-detail" >
                             <span className="time">1p</span>
-                            <button>Like ({numberOfReact})</button>
+                            <button onMouseEnter={() => setShowReactions(true)}
+                        onMouseLeave={() => setShowReactions(false)}>Like ({numberOfReact})</button>
+                            
+                                <Reaction showReactions={showReactions} />
+                            
                         </div>
                     </div>
                 </div>
