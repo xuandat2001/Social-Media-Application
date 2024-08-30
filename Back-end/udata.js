a58c388d01a5e2c59245f7462d4624ad65d5d079
MinhNguyenRMIT <88420641+MinhNguyenRMIT@users.noreply.github.com>
2024-08-30 16:36:22 +0700

    added udata.js (user data) file

---
+++ Back-end/udata.js
@@ -0,0 +1,6 @@
+const users = [
+    { username: "johndoe", password: "password123" },
+    { username: "janedoe", password: "securepass" },
+    { username: "admin", password: "adminpass" },
+    { username: "abcd", password:"1234"},
+];
