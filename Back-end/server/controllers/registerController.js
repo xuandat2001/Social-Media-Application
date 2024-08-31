d5e4e95d3c8c9c958524d1bd95100368e066ae09
TVNT99 <s3870729@rmit.edu.vn>
2024-08-31 16:43:14 +0700

    register

---
+++ Back-end/server/controllers/registerController.js
@@ -0,0 +1,29 @@
+const users = require('../udata').users;
+
+const register = (req, res, next) => {
+    const username = req.body.name;
+    const password = req.body.password;
+    const ids = users.map((p)=> p.id);
+    const maxId = Math.max(...ids);
+    const newId = maxId + 1;
+    // Check if the username already exists
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(409).json({ message: 'Username already exists.' });
+    }
+
+    // Create a new user
+    const newUser = {
+         // Generate a new ID
+        newId,
+        username,
+        password
+    };
+    users.push(newUser);
+
+    res.status(201).json({ message: 'User registered successfully!' });
+};
+
+module.exports = {
+    register
+};
--- Back-end/server/routes/users.js
+++ Back-end/server/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController')
+const registerController = require('../controllers/registerController');
 
 const router = express.Router();
 
 router.get('/', userController.get);
-
+router.post('/register', express.json(), registerController.register);
 
 exports.userRoutes = router;
