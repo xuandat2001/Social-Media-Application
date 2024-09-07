53b32b2156c7b6f8951363bbfd3b031483115cd3
quanghuy0112 <155317850+quanghuy0112@users.noreply.github.com>
2024-09-07 16:30:34 +0700

    update frontend create post

--- Front-end/src/components/User-Site/CreatePostBox.jsx
+++ Front-end/src/components/User-Site/CreatePostBox.jsx
@@ -3,57 +3,64 @@ import '../../css/postBox.css';
 import dogImage from '../../image/white-dog-names-1-modified.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faImage, faFaceSmile, faLocationDot, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../../Authentication_Context/Auth_Provider";
 
 
-function CreatePostBox(){
+
+function CreatePostBox({showCreatePostBox, closeCreatePostBox}){
+    const {user} = useAuth();
+    console.log(user);
     return (
         <>
-            <div className="container custom-container">
+        {showCreatePostBox && (
+             <div className="container custom-container">
                 
-                <div className="row headerBox add-border-bottom">
-                    <div className='col-11'>
-                        <strong>Create Post</strong>
-                    </div>
-                    <div className='col-1'>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </div>
-                </div>
+             <div className="row headerBox add-border-bottom">
+                 <div className='col-11'>
+                     <strong>Create Post</strong>
+                 </div>
+                 <div className='col-1'>
+                    <button style={{ backgroundColor: "transparent", border: "none" }} onClick={closeCreatePostBox}> <FontAwesomeIcon icon={faCircleXmark} /></button>
+                 </div>
+             </div>
 
-                <div className='row'>
-                    <div className='col-1 add-margin-right'>
-                        <img className='avatar' src={dogImage} alt='avatar'/>
-                    </div>
-                    <div className='col-6'>
-                        <h2>Quang Huy</h2>
-                        <select id = "accessRight" name="accessRight">
-                            <option value="public">Public</option>
-                            <option value="friend">Friend</option>
-                            <option value="private">Private</option>
-                        </select>
-                    </div>
-                </div>
+             <div className='row'>
+                 <div className='col-1 add-margin-right'>
+                     <img className='avatar' src={dogImage} alt='avatar'/>
+                 </div>
+                 <div className='col-6'>
+                     <h2>{user.userName}</h2>
+                     <select id = "accessRight" name="accessRight">
+                         <option value="public">Public</option>
+                         <option value="friend">Friend</option>
+                         <option value="private">Private</option>
+                     </select>
+                 </div>
+             </div>
 
-                <div className='row'>
-                    <div className='text-area-div'>
-                        <textarea className='text-area' placeholder='What are you thinking?' id="statement" name='statement' cols="65" rows="2" ></textarea>
-                    </div>
-                </div>
-                
-                <div className='row'>
-                    <div className='col-6'></div>
-                    <div className='col-6'>
-                        <nav className="nav add-border nav-insert-buttons" >
-                            <a className=" icon-color" href="#"><FontAwesomeIcon icon={faImage}/></a>
-                            <a className="icon-color" href="#"><FontAwesomeIcon icon={faFaceSmile} /></a>
-                            <a className="icon-color" href="#"><FontAwesomeIcon icon={faLocationDot} /></a>
-                            <a className="icon-color" href="#"><FontAwesomeIcon icon={faEllipsis} /></a>
-                            <button type='button' className='custom-post-button'>Post</button>                
-                        </nav>
-                    </div>
+             <div className='row'>
+                 <div className='text-area-div'>
+                     <textarea className='text-area' placeholder='What are you thinking?' id="statement" name='statement' cols="65" rows="2" ></textarea>
+                 </div>
+             </div>
+             
+             <div className='row'>
+                 <div className='col-6'></div>
+                 <div className='col-6'>
+                     <nav className="nav add-border nav-insert-buttons" >
+                         <a className=" icon-color" href="#"><FontAwesomeIcon icon={faImage}/></a>
+                         <a className="icon-color" href="#"><FontAwesomeIcon icon={faFaceSmile} /></a>
+                         <a className="icon-color" href="#"><FontAwesomeIcon icon={faLocationDot} /></a>
+                         <a className="icon-color" href="#"><FontAwesomeIcon icon={faEllipsis} /></a>
+                         <button type='button' className='custom-post-button'>Post</button>                
+                     </nav>
+                 </div>
 
-                </div>
-                 
-            </div>
+             </div>
+              
+         </div>
+        )}
+           
         </>
     )
 }
